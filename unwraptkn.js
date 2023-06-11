2adc357aa686aa93c56a0ac7592a9eeaec30993d
Jungter <59937326+Ungter@users.noreply.github.com>
2023-06-11 01:54:33 -0400

    checks wallet for amt of tokens

--- unwraptkn.js
+++ unwraptkn.js
@@ -1,7 +1,7 @@
 const ethers = require('ethers');
 const axios = require('axios');
 
-const provider = new ethers.JsonRpcProvider('https://1rpc.io/bnb');
+const provider = new ethers.JsonRpcProvider('');
 
 const tokenContractAddress = '0x7cF551258d6871b72EE1bD1624588a6245bF48c4';
 const abi = ['function balanceOf(address owner) public view returns (uint256)'];
@@ -9,41 +9,59 @@ const contract = new ethers.Contract(tokenContractAddress, abi, provider);
 
 
 const walletAddress = '0x55A1dd7eB606Da6e5A546AC62b9a9E90CB495A9A';
-const unwrapAbi = ['function unwrap(uint256 amount) public']
+const unwrapAbi = ['function unwrap(uint256 amount) public', 'function balanceOf(address owner) public view returns (uint256)']
 let lastBalance = BigInt('0');
 const unwrapperContract = new ethers.Contract(walletAddress, unwrapAbi, provider)
 
 const walletPrivateKey = '';
+const walletAddr = '';
 const wallet = new ethers.Wallet(walletPrivateKey, provider);
 
 const contractWithSigner = unwrapperContract.connect(wallet);
+let checkBalanceInterval;
 
 async function checkBalance() {
     console.log('Checking balance...')
     const balance = await contract.balanceOf(walletAddress);
+    const tokensLeftToUnwrap = await unwrapperContract.balanceOf(walletAddr);
 
-    console.log('current balance:' + ethers.formatEther(balance))
+    console.log('current contract balance:' + ethers.formatEther(balance))
+    
     if (balance > lastBalance) {
         const increase = balance - lastBalance;
         console.log('Balance increased! New balance: ' + ethers.formatEther(balance));
         notifyTelegram('Balance increased! New balance: ' + ethers.formatEther(balance));
 
-        const tx = await contractWithSigner.unwrap(increase);
-        console.log('Unwrap transaction sent, tx hash:', tx.hash);
-        notifyTelegram('Unwrap transaction sent, tx hash: ' + tx.hash);
-        notifyTelegram('Unwrapped amount: ' + ethers.formatEther(increase) + ' Chitcat');
+        if (increase <= tokensLeftToUnwrap) {
+            const tx = await contractWithSigner.unwrap(increase);
+            console.log('Unwrap transaction sent, tx hash:', tx.hash);
+            notifyTelegram('Unwrap transaction sent, tx hash: ' + tx.hash);
+            notifyTelegram('Unwrapped amount: ' + ethers.formatEther(increase) + ' Chitcat');
+            notifyTelegram('current wallet balance:' + ethers.formatEther(tokensLeftToUnwrap));
+        } else {
+            const tx = await contractWithSigner.unwrap(tokensLeftToUnwrap);
+            console.log('Unwrap transaction sent, tx hash:', tx.hash);
+            notifyTelegram('Unwrap transaction sent, tx hash: ' + tx.hash);
+            notifyTelegram('Unwrapped amount: ' + ethers.formatEther(tokensLeftToUnwrap) + ' Chitcat');
+            notifyTelegram('current wallet balance:' + ethers.formatEther(tokensLeftToUnwrap));
+        }
+        
         lastBalance = balance;
+    } else if (tokensLeftToUnwrap == 0) {
+        clearInterval(checkBalanceInterval);
+        notifyTelegram('All tokens unwrapped!');
     }
 }
 
 async function notifyTelegram(message) {
-    const url = `https://api.telegram.org/bot<BOT_ID>/sendMessage?chat_id=<CHAT_ID>&text=${encodeURIComponent(message)}`;
+    const url = `https://api.telegram.org/bot<BOT_KEY>/sendMessage<CHAT_ID>?chat_id=&text=${encodeURIComponent(message)}`;
     try {
         await axios.get(url);
     } catch (error) {
         console.error('Error sending Telegram message:', error);
     }
 }
-notifyTelegram('Starting balance checker...');
+
+notifyTelegram('Starting unwrap checker...');
 checkBalance();
-setInterval(checkBalance, 30000); // Check every 30 seconds
+checkBalanceInterval = setInterval(checkBalance, 10000);
